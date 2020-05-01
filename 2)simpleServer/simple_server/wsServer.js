ede4099229f6b7a894097d9c5eaee5e4524271ca
Locketgoma <gom@kiwing.co.kr>
2020-05-02 03:28:01 +0900

    2: echo server sample

---
+++ 2)simpleServer/simple_server/wsServer.js
@@ -0,0 +1,26 @@
+const WebSocketModule = require ("ws");
+
+module.exports = function(_server){
+	
+	const WebSocketServer = new WebSocketModule.Server({server:_server});
+	
+	WebSocketServer.on('connection',function(websocket,req){
+		let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+		console.log(ip+ "클라이언트에서 접속 요청이 있습니다.");
+		
+		websocket.on('message',function(message){
+			console.log(ip + ":" + message);
+			
+			//에코 코드
+			websocket.send("echo:"+message);
+		});
+		
+		
+		websocket.on('error',function(error){
+			console.log(ip+'클라이언트와 연결 중 오류 발생 : '+ error);
+		})
+		websocket.on('close',function(){
+			console.log(ip+'클라이언트와 접속이 해제되었습니다');
+		})
+	});
+}
